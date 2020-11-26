c55139b4b67f63b826b6906f79ca4c50b3822b4c
Cassidy Linhares <30815527+cassidylinhares@users.noreply.github.com>
2020-11-26 11:50:16 -0500

    Update menu.sql

--- Dump_yourSpot/menu.sql
+++ Dump_yourSpot/menu.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `yourSpot` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `yourSpot`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: yourSpot
