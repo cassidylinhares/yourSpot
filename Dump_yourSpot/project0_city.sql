1da75581baf3a6ab95daef4f4e7c9d14a3a5bd92
Cassidy Linhares <30815527+cassidylinhares@users.noreply.github.com>
2020-11-26 11:42:05 -0500

    Update project0_city.sql

--- Dump_yourSpot/project0_city.sql
+++ Dump_yourSpot/project0_city.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: localhost    Database: project0
+-- Host: localhost    Database: yourSpot
 -- ------------------------------------------------------
 -- Server version	8.0.22
 
